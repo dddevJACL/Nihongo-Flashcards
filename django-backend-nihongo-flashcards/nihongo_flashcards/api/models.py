db4bd4e41031c44a84bbc1f491028780ebcd552e
dddevJACL <144203852+dddevJACL@users.noreply.github.com>
2024-02-22 23:12:02 +0900

    upload backend files

---
+++ django-backend-nihongo-flashcards/nihongo_flashcards/api/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class VocabularyCard(models.Model):
+    """TODO"""
+    
+    # Fields for Vocabulary Card class
+    vocab_word = models.CharField(max_length=25, help_text='Enter a vocabulary word for this flashcard')
+
+    example_sentence = models.TextField(blank=True, help_text='Enter an example sentence for the vocabulary word')
+    
+    vocab_answer = models.TextField(help_text='Insert one or more definition(s), translation(s), etc. for your vocabulary word')
+
+    num_times_correct = models.IntegerField(default=0)
+
+    num_times_wrong = models.IntegerField(default=0)
+
+    correct_streak = models.IntegerField(default=0)
+
+    days_until_next_study = models.IntegerField(default=0, help_text='Enter an integer representing the number of days until you want to study this card again.')
+
+    date_last_studied = models.DateTimeField(auto_now=True)
+    
+    # Metadata for Vocabulary Card class
+    class Meta:
+        ordering = ['-vocab_word']
+    
+    # Methods for Vocabulary Card class
+    def get_absolute_url(self):
+        """TODO"""
+        return reverse('model-detail-view', args=[str(self.id)])
+    
+    def __str__(self):
+        """TODO"""
+        return self.vocab_word
---
+++ django-backend-nihongo-flashcards/nihongo_flashcards/api/views.py
@@ -0,0 +1,142 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import VocabularyCard
+from .serializers import VocabCardSerializer
+# import webbrowser
+import requests
+from bs4 import BeautifulSoup
+
+
+
+@api_view(['GET'])
+def get_routes(request):
+
+    #in wikiscraper
+    routes = [
+        {
+            'Endpoint': '/vocabcards/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of flashcards'
+        },
+        {
+            'Endpoint': '/vocabcard/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note flashcard'
+        },
+        {
+            'Endpoint': '/vocabcards/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates a new flashcard with data sent in post request'
+        },
+        {
+            'Endpoint': '/vocabcard/id/edit/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Updates a flashcard with data sent in post request'
+        },
+        {
+            'Endpoint': '/vocabcard/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes an exiting flashcard'
+        },
+    ]
+
+    return Response(routes)
+
+@api_view(['GET'])
+def get_all_vocabcards(request):
+    vocab_cards = VocabularyCard.objects.all().order_by('id')
+    serializer = VocabCardSerializer(vocab_cards, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_vocabcard(request, primary_key):
+    vocab_card = VocabularyCard.objects.get(id=primary_key)
+    serializer = VocabCardSerializer(vocab_card, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_all_studycards(request):
+    vocab_cards = VocabularyCard.objects.filter(days_until_next_study=0).order_by('days_until_next_study')
+    serializer = VocabCardSerializer(vocab_cards, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_all_cards_by_last_study(request):
+    vocab_cards = VocabularyCard.objects.all().order_by('date_last_studied')
+    serializer = VocabCardSerializer(vocab_cards, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_vocabCard(request):
+    data = request.data
+    try:
+        vocab_card = VocabularyCard.objects.create(
+            vocab_word=data['vocab_word'],
+            example_sentence=data['example_sentence'],
+            vocab_answer=data['vocab_answer']
+            )
+    except:
+        vocab_card = VocabularyCard.objects.create(
+            vocab_word=data['vocab_word'],
+            vocab_answer=data['vocab_answer']
+            )
+    serializer = VocabCardSerializer(vocab_card, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_definition(request, primary_key):
+    """Search jisho.org with a japanese search word"""
+    response = requests.get(f"https://jisho.org/word/{primary_key}")
+    text_soup = BeautifulSoup(response.content, 'html.parser')
+    furigana = text_soup.find("span", {"class": "furigana"})
+    furigana = "".join(furigana.get_text().split("\n"))
+    definition_list = list([f"{primary_key} 【{furigana}】"])
+    body_spans = text_soup.find_all("span", {"class": "meaning-meaning"})
+    counter = 1
+    for span_element in body_spans:
+        span_text = span_element.get_text()
+        char = ord(span_text[0])
+        if (65 <= char <= 90) or (97 <= char <= 122):
+            definition_list.append(f"{counter}. {span_text}")
+            counter += 1
+        else:
+            definition_list.append(f"Other forms: {span_text}")
+    definition = "\n".join(definition_list)
+    print(definition)
+    return Response(definition)
+
+
+@api_view(['PUT'])
+def edit_vocab_card(request, primary_key):
+    data = request.data
+    vocab_card = VocabularyCard.objects.get(id=primary_key)
+    serializer = VocabCardSerializer(instance=vocab_card, data=data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def update_day(request, days):
+    vocab_cards = VocabularyCard.objects.all()
+    for card in vocab_cards:
+        card.days_until_next_study -= int(days)
+        if card.days_until_next_study < 0:
+            card.days_until_next_study = 0
+        card.save()
+    return Response("Due dates updated")
+
+
+@api_view(['DELETE'])
+def delete_vocab_card(request, primary_key):
+    card = VocabularyCard.objects.get(id=primary_key)
+    delete_message = 'Card: ' + card.vocab_word + 'was deleted'
+    card.delete()
+    return Response(delete_message)
