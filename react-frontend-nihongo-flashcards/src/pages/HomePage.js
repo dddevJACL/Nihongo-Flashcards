fd1af806ac70e498d8f9b869fef338f83dda8884
dddevJACL <144203852+dddevJACL@users.noreply.github.com>
2024-03-04 16:36:58 +0900

    Update HomePage.js with automatic study time update

--- react-frontend-nihongo-flashcards/src/pages/HomePage.js
+++ react-frontend-nihongo-flashcards/src/pages/HomePage.js
@@ -12,29 +12,14 @@ const HomePage = () => {
     let getVocabCards = async () => {
         let response = await fetch('/api/home/')
         let data = await response.json()
+        console.log(data)
         setVocabCards(data)
     }
-
-
-  let updateDay = async () => {
-    console.log(num_days)
-    fetch(`api/home/update/${num_days}/`, {
-      method: "PUT",
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(num_days)
-  })
-
-}
-let num_days = 0
 
   return (
     <div>
       <h2 className='notes-title'>Welcome to Nihongo Flashcards!</h2>
-      <label for='days-countdown'>How many days has it been since you last used Nihongo Flashcards?: </label>
-      <input type="number" id='days-countdown' name='days-countdown' min={0} onChange={(e) => num_days = e.target.value} ></input>
-      <button onClick={updateDay} title='Update due dates of all flashcards with the given number'>Update Due Dates</button>
+      <p>Your last study session was: {vocabCards}</p>
     </div>
   )
 }
